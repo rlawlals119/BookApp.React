d186a517c86c7f3bee1f914e6e827427cab250ae
rlawlals119 <rlawlals170@naver.com>
2022-01-14 15:07:47 +0900

    first commit

---
+++ BookApp.React/ClientApp/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+
+import './custom.css'
+
+import { BooksIndex } from './components/Books/BooksIndex';
+import { BooksCreate } from './components/Books/BooksCreate';
+import { BooksEdit } from './components/Books/BooksEdit';
+import { BooksDelete } from './components/Books/BooksDelete';
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+                <Route path={['/Books', '/Books/Index']} component={BooksIndex} exact />
+                <Route path='/Books/Create' component={BooksCreate} exact={true} />
+                <Route path='/Books/Edit/:id' component={BooksEdit} exact />
+                <Route path='/Books/Delete/:id' component={BooksDelete} exact />
+            </Layout>
+        );
+    }
+}
